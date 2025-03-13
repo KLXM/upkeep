ba491986a60cb2ab8cc580bdcc0a856cffb3aff2
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-13 11:04:06 +0100

    Update domains.php

--- pages/domains.php
+++ pages/domains.php
@@ -17,22 +17,29 @@ if (!rex_addon::get('yrewrite')->isAvailable()) {
 $domains = rex_yrewrite::getDomains();
 
 // Aktuelle Einstellungen laden
-$allowedDomains = (array) $addon->getConfig('allowed_domains', []);
 $domainStatus = (array) $addon->getConfig('domain_status', []);
+$allDomainsLocked = (bool) $addon->getConfig('all_domains_locked', false);
 
 // Formular abgesendet?
 if (rex_post('save', 'boolean')) {
-    // Einstellungen speichern
-    $domainStatus = [];
-    foreach ($domains as $domain) {
-        $name = $domain->getName();
-        if ($name !== 'default') {
-            $domainStatus[$name] = rex_post('domain_' . md5($name), 'boolean', false);
-        }
-    }
+    // Globale Option für alle Domains speichern
+    $allDomainsLocked = rex_post('all_domains_locked', 'boolean', false);
+    $addon->setConfig('all_domains_locked', $allDomainsLocked);
     
-    // Konfiguration speichern
-    $addon->setConfig('domain_status', $domainStatus);
+    // Nur wenn nicht alle Domains gesperrt sind, individuelle Einstellungen speichern
+    if (!$allDomainsLocked) {
+        // Einstellungen speichern
+        $domainStatus = [];
+        foreach ($domains as $domain) {
+            $name = $domain->getName();
+            if ($name !== 'default') {
+                $domainStatus[$name] = rex_post('domain_' . md5($name), 'boolean', false);
+            }
+        }
+        
+        // Konfiguration speichern
+        $addon->setConfig('domain_status', $domainStatus);
+    }
     
     // Erfolgsmeldung
     echo rex_view::success($addon->i18n('upkeep_settings_saved'));
@@ -40,6 +47,20 @@ if (rex_post('save', 'boolean')) {
 
 // Tabelle mit Domains erstellen
 $content = '<form action="' . rex_url::currentBackendPage() . '" method="post">';
+
+// Option für alle Domains
+$content .= '<div class="form-group">';
+$content .= '<label class="control-label">' . $addon->i18n('upkeep_lock_all_domains') . '</label>';
+$content .= '<div class="rex-select-style">';
+$content .= '<select class="form-control" name="all_domains_locked" id="all-domains-locked">';
+$content .= '<option value="0"' . (!$allDomainsLocked ? ' selected' : '') . '>' . $addon->i18n('upkeep_no') . '</option>';
+$content .= '<option value="1"' . ($allDomainsLocked ? ' selected' : '') . '>' . $addon->i18n('upkeep_yes') . '</option>';
+$content .= '</select>';
+$content .= '</div>';
+$content .= '</div>';
+
+// Individuelle Domain-Einstellungen
+$content .= '<div id="individual-domains" ' . ($allDomainsLocked ? 'style="display:none;"' : '') . '>';
 $content .= '<table class="table table-striped table-hover">';
 $content .= '<thead><tr>';
 $content .= '<th>' . $addon->i18n('upkeep_domain') . '</th>';
@@ -65,6 +86,8 @@ foreach ($domains as $domain) {
 }
 
 $content .= '</tbody></table>';
+$content .= '</div>';
+
 $content .= '<div class="form-group">';
 $content .= '<button class="btn btn-primary" type="submit" name="save" value="1">' . $addon->i18n('upkeep_save') . '</button>';
 $content .= '</div>';
@@ -81,3 +104,17 @@ $fragment->setVar('class', 'edit', false);
 $fragment->setVar('title', $addon->i18n('upkeep_domains_management'), false);
 $fragment->setVar('body', $notice . $content, false);
 echo $fragment->parse('core/page/section.php');
+
+// JavaScript für die Umschaltung der individuellen Domain-Einstellungen
+?>
+<script type="text/javascript">
+$(document).on('rex:ready', function() {
+    $('#all-domains-locked').on('change', function() {
+        if ($(this).val() == '1') {
+            $('#individual-domains').hide();
+        } else {
+            $('#individual-domains').show();
+        }
+    });
+});
+</script>
