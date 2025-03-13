89122098c66d0c8c7e49cfa096f7a9e63ae3b506
Thomas Skerbis <thomas.skerbis@mac.com>
2025-03-13 10:00:48 +0100

    1.0.0

--- boot.php
+++ boot.php
@@ -1,1 +1,32 @@
-<?php // Boot code
+<?php
+/**
+ * Boot-Datei für das Upkeep AddOn
+ */
+
+use KLXM\Upkeep\Upkeep;
+
+// Falls Setup aktiv ist, nichts tun
+if (rex::isSetup()) {
+    return;
+}
+
+// Register Extension Point nach dem Laden aller Packages
+rex_extension::register('PACKAGES_INCLUDED', static function () {
+    // Frontend-Prüfung
+    if (rex::isFrontend()) {
+        Upkeep::checkFrontend();
+    }
+    
+    // Backend-Prüfung
+    if (rex::isBackend()) {
+        if (rex::getUser()) {
+            Upkeep::checkBackend();
+        }
+        
+        // Statusindikator im Backend-Menü setzen
+        Upkeep::setStatusIndicator();
+        
+        // CSS für das Backend laden
+        rex_view::addCssFile(rex_addon::get('upkeep')->getAssetsUrl('css/upkeep.css'));
+    }
+});
